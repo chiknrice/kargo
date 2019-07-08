35d69082c61015166667469a29f90a4450d31475
ian <chiknrice@gmail.com>
2019-07-08 23:38:29 +1200

    wrap copyright and fixed some style issues

--- core/src/main/kotlin/org/chiknrice/kargo/CodecContextDsl.kt
+++ core/src/main/kotlin/org/chiknrice/kargo/CodecContextDsl.kt
@@ -3,9 +3,13 @@
  *
  * This file is part of project "kargo"
  *
- * Project kargo is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or(at your option) any later version.
+ * Project kargo is free software: you can redistribute it and/or modify it under the terms of the GNU General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or(at your option)
+ * any later version.
  *
- * Project kargo is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * Project kargo is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+ * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public
+ * License for more details.
  *
  * You should have received a copy of the GNU General Public License along with this program. If not, see
  * <http://www.gnu.org/licenses/>.
@@ -33,7 +37,7 @@ typealias ConfigSpec<T> = T.() -> Unit
 internal class ConfigTemplate<T : Any>(private val configClass: KClass<T>, private val configSpec: ConfigSpec<T>) {
     fun createNew(): T = try {
         configClass.createInstance().apply(configSpec)
-    } catch (e: Exception) {
+    } catch (e: IllegalArgumentException) {
         throw ConfigurationException("Invalid configuration class ${configClass.qualifiedName}", e)
     }
 }
